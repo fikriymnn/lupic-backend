89ebaddc226c5d7f258a4f43efe0e80307cad283
luthfikhaerii <luthfikhaeri00@gmail.com>
2025-12-11 02:22:22 +0700

    update bug home content

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -9,7 +9,7 @@ const home_controller = {
                 success: true,
                 hero_section: data[0].hero_section,
                 carousel: data[0].carousel,
-                hero_partner: data[0].carousel,
+                hero_partner: data[0].hero_partner,
                 hero_title:data[0].hero_title,
                 hero_description:data[0].hero_description,
                 footer_partner: data[0].footer_partner
@@ -43,6 +43,7 @@ const home_controller = {
             await Home.updateOne({ _id: id }, { hero_section, carousel,hero_partner,footer_partner,hero_description,hero_title })
             res.send("success")
         } catch (err) {
+            console.log(err.message)
             res.status(500).json({
                 message: err.message
             })
